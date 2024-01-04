38e1a99ba86618ed79ba413affc7130e49ad61bf
RomaSamkov <Romansamkov.rs@gmail.com>
2024-01-04 19:56:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { log } from "console";
-import { register } from "module";
+import { register } from "./controllers/auth.js";
 
 /* ------- Configurations ------- */
 
@@ -20,6 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
