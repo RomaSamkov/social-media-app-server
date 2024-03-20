94b1c44f71dad3661008a7920be93c3869cc6b42
RomaSamkov <Romansamkov.rs@gmail.com>
2024-03-20 12:41:52 +0200

    add userRoutes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 /* ------- Configurations ------- */
@@ -50,6 +51,7 @@ app.post("/auth/register", upload.single("picture"), register);
 /* ------- Routes ------- */
 
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /* ------- Mongoose Setup ------- */
 
