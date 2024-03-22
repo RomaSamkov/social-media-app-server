0fad13fff0beba92de6087d66015edf4c43b2515
RomaSamkov <Romansamkov.rs@gmail.com>
2024-03-22 11:33:03 +0200

    add getUserPosts

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { getFeedPosts } from "../controllers/posts.js";
+import { getFeedPosts, getUserPosts } from "../controllers/posts.js";
 
 const router = express.Router();
 
 /* ------- Read ------- */
 
 router.get("/", verifyToken, getFeedPosts);
-router.get("/:userId/posts", verifyToken);
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
 /* ------- Update ------- */
 
